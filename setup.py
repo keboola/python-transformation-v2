44f38a3b72977acb16f7a7b15d6dae2846a2e3a6
marc <marc@keboola.com>
2023-11-13 13:58:49 +0100

    use diff setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='kbc_transformation',
-    version='1.0',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     url='https://github.com/keboola/python-transformation-v2',
     packages=find_packages(exclude=['tests']),
-    requires=['pip']
+    requires=['pip'],
+    license="MIT"
 )
